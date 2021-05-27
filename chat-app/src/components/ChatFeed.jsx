1276b197e2611adf46b40a3b51473c4b0f4698ed
Devin MacGillivray <devhmac@gmail.com>
2021-05-27 15:34:21 -0600

    building out render messages function to map through messages

--- chat-app/src/components/ChatFeed.jsx
+++ chat-app/src/components/ChatFeed.jsx
@@ -1,5 +1,35 @@
+import MessageForm from "./MessageForm";
+import Message from "./Message";
+import IncomingMessage from "./IncomingMessage";
+
 const ChatFeed = (props) => {
-  console.log(props);
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  console.log(messages);
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    console.log("keys", keys);
+    return keys.map((key, index) => {
+      const message = messages[key];
+
+      //if there are messages, find the last one
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? <Message /> : <IncomingMessage />}
+          </div>
+        </div>
+      );
+    });
+  };
+
+  renderMessages();
 
   return <div>ChatFeed</div>;
 };
