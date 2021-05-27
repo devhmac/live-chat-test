225124d524de766a7634012ef5b4a38f39981203
Devin MacGillivray <devhmac@gmail.com>
2021-05-27 17:51:58 -0600

    fixes message conditional checking if image or text

--- chat-app/src/components/IncomingMessage.jsx
+++ chat-app/src/components/IncomingMessage.jsx
@@ -12,25 +12,26 @@ const Message = ({ lastMessage, message }) => {
         />
       )}
       {/* if its image render image, else render message */}
-      {/* {message?.attachments?.length > 0} ? (
-      <img
-        src={message.attachments[0].filename}
-        alt="message-attachment"
-        className="message-image"
-        style={{ marginleft: userFirstMessage ? "4px" : "48px" }}
-      />
-      ) : ( */}
-      <div
-        className="message"
-        style={{
-          float: "left",
-          color: "white",
-          marginleft: userFirstMessage ? "4px" : "48px",
-          backgroundColor: "#CABCDC",
-        }}
-      >
-        {message.text}
-      </div>
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].filename}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginleft: userFirstMessage ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            color: "white",
+            marginleft: userFirstMessage ? "4px" : "48px",
+            backgroundColor: "#CABCDC",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
     </div>
   );
 };
