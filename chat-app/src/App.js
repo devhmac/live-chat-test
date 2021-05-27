9f806ff94f5445ebe2603a9d61c2aed372c99d93
Devin MacGillivray <devhmac@gmail.com>
2021-05-27 15:07:14 -0600

    imports and uses chat engine for base"

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="8b2efd59-f1b9-458d-89d4-616c2e6e6713"
+      userName="devmac"
+      userSecret="password"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
