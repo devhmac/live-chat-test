cca0a8813c7f2bb12c7a5d4b44a8c1cf7b095e19
Devin MacGillivray <devhmac@gmail.com>
2021-05-27 16:41:38 -0600

    message component built

--- chat-app/src/components/Message.jsx
+++ chat-app/src/components/Message.jsx
@@ -1,5 +1,28 @@
-const Message = () => {
-  return <div>My Message</div>;
+const Message = ({ message }) => {
+  // checking if message is image
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].filename}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#3B2A50",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 };
 
 export default Message;
