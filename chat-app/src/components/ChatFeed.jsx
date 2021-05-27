91b80937f32d754d4a91593170537df45bbe6b10
Devin MacGillivray <devhmac@gmail.com>
2021-05-27 15:56:49 -0600

    chat feed built out to map messages and render them in the component

--- chat-app/src/components/ChatFeed.jsx
+++ chat-app/src/components/ChatFeed.jsx
@@ -4,19 +4,18 @@ import IncomingMessage from "./IncomingMessage";
 
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
-
+  console.log(chats);
+  //if chat exists, and checks which chat you're actively looking at
   const chat = chats && chats[activeChat];
-
-  console.log(messages);
 
   const renderMessages = () => {
     const keys = Object.keys(messages);
-    console.log("keys", keys);
     return keys.map((key, index) => {
       const message = messages[key];
 
       //if there are messages, find the last one
       const lastMessageKey = index === 0 ? null : keys[index - 1];
+      // check if username = username of sender
       const isMyMessage = userName === message.sender.username;
 
       return (
@@ -24,14 +23,38 @@ const ChatFeed = (props) => {
           <div className="message-block">
             {isMyMessage ? <Message /> : <IncomingMessage />}
           </div>
+          <div
+            className="read-reciepts"
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          >
+            read-reciepts
+          </div>
         </div>
       );
     });
   };
 
-  renderMessages();
+  if (!chat) return "Loading....";
 
-  return <div>ChatFeed</div>;
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {/* makes subtitle a string of all usernames in the chat */}
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages}
+      <div style={{ height: "100px" }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
 };
 
 export default ChatFeed;
