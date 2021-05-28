a2d5379464e84fcf837abfdfc5626b7b9bbe6709
Devin MacGillivray <devhmac@gmail.com>
2021-05-27 20:22:45 -0600

    testing user change

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <ChatEngine
       height="100vh"
       projectID="8b2efd59-f1b9-458d-89d4-616c2e6e6713"
-      userName="devmac"
+      userName="luke"
       userSecret="password"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
